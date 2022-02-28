5689d13ab9648503d44372407b1ad43aa67cc34d
Suleym Yovloy <suleymevlo81@gmail.com>
2022-02-28 11:21:40 +0300

    Сделал get запрос на сервер

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,7 @@
 import * as axios from "axios";
 import React from "react";
 import styles from "./Users.module.css";
-import userPhoto from "../../assets/images/user.png";
+import userPhoto from "../../assets/images/user.jpg";
 
 let Users = (props) => {
   if (props.users.length === 0) {
